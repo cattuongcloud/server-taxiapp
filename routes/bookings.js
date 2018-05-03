87a3d3def81d8c7d9b4c98ca33ce99737b04f973
unknown <cattuong.cloud@gmail.com>
2018-05-03 11:15:51 +0700

    edit bookings

--- routes/bookings.js
+++ routes/bookings.js
@@ -4,7 +4,6 @@ var mongojs = require("mongojs");
 
 //var db = mongojs("mongodb://eman:eman@ds163181.mlab.com:63181/taxiapp", ["bookings"]);
 var db = mongojs("mongodb://tuonghuynh:tuong123@ds111390.mlab.com:11390/taxiapp", ["bookings"]);
-
 router.get("/bookings", function(req, res, next){
 	db.bookings.find(function(err, bookings){
 		if(err){
@@ -17,20 +16,22 @@ router.get("/bookings", function(req, res, next){
 
 router.post("/bookings", function(req, res, next){
 	var booking = req.body.data;
-	console.log(booking);
-	// if(!booking.userName){
-	// 	res.status(400);
-	// 	res.json({
-	// 		error:"Bad data"
-	// 	});	
-	// } else {
-	// 	db.bookings.save(booking, function(err, savedBooking){
-	// 		if(err){
-	// 			res.send(err);
-	// 		}
-	// 		res.json(savedBooking);			
-	// 	});
-	// }
+	var nearByDriver = req.body.nearByDriver;
+	var io = req.app.io;
+
+	if(!booking.userName){
+		res.status(400);
+		res.json({
+			error:"Bad data"
+		});	
+	} else {
+		db.bookings.save(booking, function(err, savedBooking){
+			if(err){
+				res.send(err);
+			}
+			res.json(savedBooking);			
+		});
+	}
 });
 
 // Driver  Update Booking done on driver side
