43ff84c5d4d5a8be49071a2996affada4d3f73ea
unknown <cattuong.cloud@gmail.com>
2018-05-03 10:36:22 +0700

    first commit

---
+++ routes/bookings.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var router = express.Router();
+var mongojs = require("mongojs");
+
+//var db = mongojs("mongodb://eman:eman@ds163181.mlab.com:63181/taxiapp", ["bookings"]);
+var db = mongojs("mongodb://tuonghuynh:tuong123@ds111390.mlab.com:11390/taxiapp", ["bookings"]);
+
+router.get("/bookings", function(req, res, next){
+	db.bookings.find(function(err, bookings){
+		if(err){
+			res.send(err);
+
+		}
+		res.json(bookings);
+	})
+}); 
+
+router.post("/bookings", function(req, res, next){
+	var booking = req.body.data;
+	console.log(booking);
+	// if(!booking.userName){
+	// 	res.status(400);
+	// 	res.json({
+	// 		error:"Bad data"
+	// 	});	
+	// } else {
+	// 	db.bookings.save(booking, function(err, savedBooking){
+	// 		if(err){
+	// 			res.send(err);
+	// 		}
+	// 		res.json(savedBooking);			
+	// 	});
+	// }
+});
+
+// Driver  Update Booking done on driver side
+router.put("/bookings/:id", function(req, res, next){
+    var io = req.app.io;
+    var booking = req.body;
+    if (!booking.status){
+        res.status(400);
+        res.json({
+            "error":"Bad Data"
+        });
+    } else {
+        db.bookings.update({_id: mongojs.ObjectId(req.params.id)},{ $set: { 
+        	driverId: booking.driverId,
+        	status: booking.status 
+        }}, function(err, updatedBooking){
+        if (err){
+            res.send(err);
+        }
+        if (updatedBooking){
+            //Get Confirmed booking
+            db.bookings.findOne({_id:  mongojs.ObjectId(req.params.id)},function(error, confirmedBooking){
+                if (error){
+                    res.send(error);
+                }
+                res.send(confirmedBooking);
+                io.emit("action", {
+                    type:"BOOKING_CONFIRMED",
+                    payload:confirmedBooking
+                });
+            });
+        }
+    });
+    }
+});
+
+
+
+
+module.exports = router;
